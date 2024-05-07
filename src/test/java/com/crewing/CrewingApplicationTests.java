27dcec81188bbe6dacf86e8f48a26f7d32829d69
gosu <q63530@naver.com>
2024-05-07 17:33:54 +0900

    initial commit

---
+++ src/test/java/com/crewing/CrewingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crewing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrewingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
