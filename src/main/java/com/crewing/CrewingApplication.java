549aa4f55a2e190df31c9a1ac709b7d8da14e7d5
gosu <q63530@naver.com>
2024-05-07 17:33:54 +0900

    initial commit

---
+++ src/main/java/com/crewing/CrewingApplication.java
@@ -0,0 +1,13 @@
+package com.crewing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrewingApplication.class, args);
+    }
+
+}
