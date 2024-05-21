00ae036da4f28096b5c423133084598d196842ba
gosu <q63530@naver.com>
2024-05-21 10:19:16 +0900

    [feat] : Admin User 정보 관리 기능 구현

--- src/main/java/com/crewing/admin/api/AdminController.java
+++ src/main/java/com/crewing/admin/api/AdminController.java
@@ -1,13 +1,41 @@
 package com.crewing.admin.api;
 
+import com.crewing.admin.service.AdminService;
+import com.crewing.user.dto.UserDTO.UserInfoResponse;
+import com.crewing.user.dto.UserDTO.UserInfoResponses;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//todo : 동아리 관련 관리 기능 (신청 받기 , 동아리 리스트 조회 등등)
+@Tag(name = "관리자", description = "관리자 API")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/admin")
 @Slf4j
 public class AdminController {
+    private final AdminService adminService;
+
+    @Operation(summary = "모든 유저 조회", description = "모든 유저를 조회한다")
+    @GetMapping("/users")
+    public ResponseEntity<UserInfoResponses> getAllUserInfo() {
+        UserInfoResponses responses = adminService.getAllUserInfo();
+
+        return ResponseEntity.ok().body(responses);
+    }
+
+    @Operation(summary = "특정 유저 조회", description = "특정 유저를 조회한다")
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<UserInfoResponse> getAllUserInfo(@PathVariable Long userId) {
+        UserInfoResponse response = adminService.getUserInfo(userId);
+
+        return ResponseEntity.ok().body(response);
+    }
+
 }
