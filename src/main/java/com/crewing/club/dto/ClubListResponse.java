c3a77a758c781610e0294ed41a6308e54bd017c9
신은혜 <elmo@sin-eunhyes-MacBook-Pro.local>
2024-05-24 20:14:41 +0900

    [feat] club : 동아리 리스트 조회 Response 구현

--- src/main/java/com/crewing/club/dto/ClubListResponse.java
+++ src/main/java/com/crewing/club/dto/ClubListResponse.java
@@ -1,2 +1,14 @@
-package com.crewing.club.dto;public class ClubListResponse {
+package com.crewing.club.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+
+@Getter
+@Builder
+public class ClubListResponse {
+    private int pageNum;
+    private int pageSize;
+    private int totalCnt;
+    private Page<ClubInfoResponse> clubs;
 }
