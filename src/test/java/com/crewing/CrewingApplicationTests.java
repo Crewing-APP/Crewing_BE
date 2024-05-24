c758647e7d54dd519305b0f621e42f376178cb6b
gosu <q63530@naver.com>
2024-05-24 09:48:17 +0900

    [fix] : test 주석처리

--- src/test/java/com/crewing/CrewingApplicationTests.java
+++ src/test/java/com/crewing/CrewingApplicationTests.java
@@ -1,13 +1,10 @@
 package com.crewing;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class CrewingApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class CrewingApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
