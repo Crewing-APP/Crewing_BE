c54fd6b522286cb17cccb4c15561bf7379b558e2
신은혜 <elmo@sin-eunhyes-MacBook-Pro.local>
2024-05-24 20:20:28 +0900

    [feat] club : Repository 페이징 처리

--- src/main/java/com/crewing/club/repository/ClubRepository.java
+++ src/main/java/com/crewing/club/repository/ClubRepository.java
@@ -1,6 +1,9 @@
 package com.crewing.club.repository;
 
+import com.crewing.club.dto.ClubInfoResponse;
 import com.crewing.club.entity.Club;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +12,6 @@ import java.util.Optional;
 
 @Repository
 public interface ClubRepository extends JpaRepository<Club,Long> {
-    List<Club> findAllByCategory(int category);
-
+    Page<Club> findAll(int category, Pageable pageable);
+    Page<Club> findAllByCategory(int category, Pageable pageable);
 }
