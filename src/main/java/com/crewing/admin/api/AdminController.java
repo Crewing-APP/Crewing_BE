52fa3bca5c5a38b30ba149e487f463a9c5b7e2c1
gosu <q63530@naver.com>
2024-06-02 20:13:24 +0900

    refactor : 생성대기 클럽 조회 및 수정 -> admin 으로 이동

--- src/main/java/com/crewing/admin/api/AdminController.java
+++ src/main/java/com/crewing/admin/api/AdminController.java
@@ -1,16 +1,28 @@
 package com.crewing.admin.api;
 
 import com.crewing.admin.service.AdminService;
+import com.crewing.auth.entity.PrincipalDetails;
+import com.crewing.club.dto.ClubChangeStatusRequest;
+import com.crewing.club.dto.ClubCreateResponse;
+import com.crewing.club.dto.ClubListResponse;
+import com.crewing.club.service.ClubReadService;
+import com.crewing.club.service.ClubService;
 import com.crewing.user.dto.UserDTO.UserInfoResponse;
 import com.crewing.user.dto.UserDTO.UserInfoResponses;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 //todo : 동아리 관련 관리 기능 (신청 받기 , 동아리 리스트 조회 등등)
@@ -21,6 +33,8 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class AdminController {
     private final AdminService adminService;
+    private final ClubReadService clubReadService;
+    private final ClubService clubService;
 
     @Operation(summary = "모든 유저 조회", description = "모든 유저를 조회한다")
     @GetMapping("/users")
@@ -36,5 +50,24 @@ public class AdminController {
         UserInfoResponse response = adminService.getUserInfo(userId);
 
         return ResponseEntity.ok().body(response);
+    }
+
+
+    @Operation(summary = "수락 신청 동아리 목록 조회", description = "WAIT 상태인 동아리 목록 조회, 페이징으로 조회 가능, 관리자만 조회")
+    @GetMapping("/clubs")
+    public ResponseEntity<ClubListResponse> getAllWaitClub(@PageableDefault(size = 10) Pageable pageable,
+                                                           @RequestParam String status,
+                                                           @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        ClubListResponse clubList = clubReadService.getAllStatusClubInfo(pageable, status,
+                principalDetails.getUser());
+        return ResponseEntity.ok().body(clubList);
+    }
+
+    @Operation(summary = "동아리 상태 변경", description = "동아리 상태 변경(WAIT,HOLD,RETURN,ACCEPT) , 관리자만 가능")
+    @PatchMapping("/clubs")
+    public ResponseEntity<ClubCreateResponse> changeStatus(@RequestBody ClubChangeStatusRequest clubChangeStatusRequest,
+                                                           @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        ClubCreateResponse clubInfo = clubService.changeStatus(clubChangeStatusRequest, principalDetails.getUser());
+        return ResponseEntity.ok().body(clubInfo);
     }
 }
