761c76ddaf7c4ff6459d623b8b43b941c4be050e
bandalgomsu <q63530@naver.com>
2024-06-28 13:22:13 +0900

    feat : 메일 전송 기능 구현

---
+++ src/main/java/com/crewing/mail/EmailDTO.java
@@ -0,0 +1,50 @@
+package com.crewing.mail;
+
+import com.example.jolvre.auth.dto.SignUpDTO.TokenResponse;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+public class EmailDTO {
+
+    @Getter
+    @AllArgsConstructor
+    @Builder
+    public static class EmailSendResponse {
+        private String email;
+        private String authNum;
+        private boolean isUser;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class EmailVerifyRequest {
+        @Email
+        @NotEmpty(message = "이메일을 입력해 주세요")
+        private String email;
+
+        @NotEmpty(message = "인증 번호를 입력해 주세요")
+        private String authNum;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @Builder
+    public static class SignUpEmailVerifyResponse {
+        private String email;
+
+        private boolean verifyResult;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @Builder
+    public static class FindPwEmailVerifyResponse {
+        private String email;
+        private boolean verifyResult;
+        private TokenResponse tokenResponse;
+    }
+
+}
