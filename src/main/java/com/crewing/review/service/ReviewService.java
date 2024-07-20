4ac8733b7596ea2de284833cf12a0d1950eb86c7
HideOnCodec <tlsdmsgp33@naver.com>
2024-07-20 18:25:17 +0900

    [FEAT] 리뷰 수정 기능 추가

--- src/main/java/com/crewing/review/service/ReviewService.java
+++ src/main/java/com/crewing/review/service/ReviewService.java
@@ -3,6 +3,7 @@ package com.crewing.review.service;
 import com.crewing.review.dto.ReviewCreateRequest;
 import com.crewing.review.dto.ReviewListResponse;
 import com.crewing.review.dto.ReviewResponse;
+import com.crewing.review.dto.ReviewUpdateRequest;
 import com.crewing.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,5 +11,6 @@ import org.springframework.data.domain.Pageable;
 public interface ReviewService {
     ReviewResponse createReview(ReviewCreateRequest createRequest, User user);
     void deleteReview(Long reviewId, User user);
+    ReviewResponse updateReview(ReviewUpdateRequest request, Long reviewId, User user);
     ReviewListResponse getAllReviewInfo(Pageable pageable, Long clubId);
 }
