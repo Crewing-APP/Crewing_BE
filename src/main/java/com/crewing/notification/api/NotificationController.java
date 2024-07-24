e11c61a5f5088e47667c828da093662a230223d8
HideOnCodec <tlsdmsgp33@naver.com>
2024-07-25 01:52:06 +0900

    [FEAT] swagger에 알림 타입 설명 추가

--- src/main/java/com/crewing/notification/api/NotificationController.java
+++ src/main/java/com/crewing/notification/api/NotificationController.java
@@ -35,7 +35,9 @@ public class NotificationController {
         return ResponseEntity.ok(SSEService.subscribe(principalDetails.getUser(), lastEventId));
     }
 
-    @Operation(summary = "사용자 알림 목록 조회",description = "로그인한 사용자의 모든 알림 조회, 페이징 처리")
+    @Operation(summary = "사용자 알림 목록 조회",description = "로그인한 사용자의 모든 알림 조회, \n" +
+            "알림 타입 : DOC_RESULT(서류 발표),FINAL_RESULT_FAIL(최종 발표 탈락),FINAL_RESULT_PASS(최종 발표 합격),CLUB_ACCEPT(동아리 승인),CLUB_RETURN(동아리 반려)\n"+
+            "페이징 처리\n")
     @GetMapping("/notifications")
     public ResponseEntity<NotificationListResponse> getAllNotificationInfo(@PageableDefault(size = 10) Pageable pageable, @AuthenticationPrincipal PrincipalDetails principalDetails) {
         NotificationListResponse notificationListResponse = notificationServiceImpl.getAllNotificationInfo(principalDetails.getUser(),pageable);
