a4757528ead1296e259ad3763d92d4d043268b3e
반달곰수 <121839239+bandalgomsu@users.noreply.github.com>
2024-07-26 14:40:04 +0900

    Feat/logging : heath check url은 로그 출력하지 않게 수정 (#34)
    * feat : 요청 로그 구현
    
    - method url code agent 로 구성
    
    * fix : heath check url은 로그 출력하지 않게 수정

--- src/main/java/com/crewing/common/config/WebConfig.java
+++ src/main/java/com/crewing/common/config/WebConfig.java
@@ -13,6 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new LogInterceptor())
                 .order(1)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/error");
+                .excludePathPatterns("/error", "/");
     }
 }
