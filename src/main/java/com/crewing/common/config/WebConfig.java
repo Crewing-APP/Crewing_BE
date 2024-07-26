f56e1d99ec3c0e41f8720e68384e434b67ad581d
bandalgomsu <q63530@naver.com>
2024-07-26 14:09:33 +0900

    feat : 요청 로그 구현
    - method url code agent 로 구성

---
+++ src/main/java/com/crewing/common/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.crewing.common.config;
+
+import com.crewing.common.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/error");
+    }
+}
