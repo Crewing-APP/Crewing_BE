7642ad9554fcac24e5d6f88fffa87631116dfb1a
bandalgomsu <q63530@naver.com>
2024-09-26 22:34:15 +0900

    fix : request body 추가

--- src/main/java/com/crewing/device/api/DeviceController.java
+++ src/main/java/com/crewing/device/api/DeviceController.java
@@ -12,15 +12,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "디바이스" , description = "디바이스 정보를 조회 및 관리 합니다")
+@Tag(name = "디바이스", description = "디바이스 정보를 조회 및 관리 합니다")
 @RestController
 @Slf4j
 @RequiredArgsConstructor
@@ -28,24 +28,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class DeviceController {
     private final DeviceService deviceService;
 
-    @Operation(summary = "나의 디바이스 정보 조회",description = "나의 디바이스 정보를 조회합니다")
+    @Operation(summary = "나의 디바이스 정보 조회", description = "나의 디바이스 정보를 조회합니다")
     @GetMapping
-    public ResponseEntity<List<DeviceInfoResponse>> getDeviceInfoByLoginUser(@AuthenticationPrincipal PrincipalDetails principalDetails){
+    public ResponseEntity<List<DeviceInfoResponse>> getDeviceInfoByLoginUser(
+            @AuthenticationPrincipal PrincipalDetails principalDetails) {
         return ResponseEntity.ok(deviceService.getDevicesInfo(principalDetails.getId()));
     }
 
-    @Operation(summary = "나의 디바이스 정보 등록",description = "나의 디바이스 정보를 등록합니다")
+    @Operation(summary = "나의 디바이스 정보 등록", description = "나의 디바이스 정보를 등록합니다")
     @PostMapping
     public ResponseEntity<DeviceInfoResponse> registryDevice(@AuthenticationPrincipal PrincipalDetails principalDetails,
-                                                             DeviceRegisterRequest request){
-        return ResponseEntity.ok(deviceService.registryDevice(request,principalDetails.getId()));
+                                                             @RequestBody DeviceRegisterRequest request) {
+        return ResponseEntity.ok(deviceService.registryDevice(request, principalDetails.getId()));
     }
 
-    @Operation(summary = "디바이스 정보 갱신",description = "나의 디바이스 정보를 갱신합니다")
+    @Operation(summary = "디바이스 정보 갱신", description = "나의 디바이스 정보를 갱신합니다")
     @PatchMapping("/{deviceId}")
     public ResponseEntity<DeviceInfoResponse> updateDevice(@AuthenticationPrincipal PrincipalDetails principalDetails,
-                                                           DeviceUpdateRequest request,
-                                                           @PathVariable Long deviceId){
-        return ResponseEntity.ok(deviceService.updateDevice(request,deviceId,principalDetails.getId()));
+                                                           @RequestBody DeviceUpdateRequest request,
+                                                           @PathVariable Long deviceId) {
+        return ResponseEntity.ok(deviceService.updateDevice(request, deviceId, principalDetails.getId()));
     }
 }
